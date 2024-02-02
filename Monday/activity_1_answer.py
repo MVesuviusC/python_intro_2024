d6e61fbb60d15af3321c57064103078b115a934c
MVesuviusC <matthewvc1@gmail.com>
2024-02-02 16:29:36 -0500

    Add Monday activity answers

---
+++ Monday/activity_1_answer.py
@@ -0,0 +1,13 @@
+my_int = 4
+print(my_int)
+
+my_double = 5.0
+print(my_double)
+
+my_bool = True
+print(my_bool)
+
+my_string = "Hello"
+print(my_string)
+
+# Run this script by typing python Monday/activity_1_answer.py
